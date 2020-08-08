5b1806779eb3fd035f45c203e82ba2cda8c1acbc
Alex Mata <alexmata92@ymail.com>
2020-08-08 15:47:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -207,7 +207,7 @@ def big_shoe_rebounds
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
       
-      if data = :players
+      if data = [:players]
         
         data.each do |player_info, value|
           binding.pry 
